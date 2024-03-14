ac8a6b84e4d09deb287affb8b9810aeda9df687b
coder-002 <marcosprajapati@gmail.com>
2024-03-14 11:52:01 +0545

    testing

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,7 +2,7 @@ import { Spinner } from "@fluentui/react-components";
 
 const Loading = () => {
   return (
-    <div className=" h-screen w-screen flex justify-center items-center">
+    <div className=" h-screen w-full flex justify-center items-center">
       <Spinner size="extra-large" labelPosition="below" label={"Loading..."} />
     </div>
   );
